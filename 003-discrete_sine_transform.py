d3b77efcfff75c9f16e7df53afb93b92e974ef2e
TheBlueChameleon <stefan_hartinger@gmx.de>
2021-02-21 18:20:51 +0100

    003 - minor changes, probably just experiments

--- 003-discrete_sine_transform.py
+++ 003-discrete_sine_transform.py
@@ -10,17 +10,30 @@ Y = np.sin(np.pi * X) + .3 * np.sin(5 * np.pi * X)
 A = scipy.fft.dst(Y, norm="ortho")
 W = [k / (P * np.pi) for k in range(len(A))]
 
-spectrumLimit = 100
+spectrumLimit = 30
 
 fig = plt.figure( figsize=(5, 10) )
-drw = fig.add_subplot(211)
+drw = fig.add_subplot(411)
 drw.set_xlabel("time t")
 drw.set_ylabel("signal intensity")
 drw.plot(X, Y)
 
-drw = fig.add_subplot(212)
+drw = fig.add_subplot(412)
 drw.set_xlabel("angular frequency $\omega$")
 drw.set_ylabel("signal amplitude")
 drw.plot(W[:spectrumLimit], A[:spectrumLimit])
+
+A[30:] = 0
+reconstruction = scipy.fft.idst(A, norm="ortho")
+
+drw = fig.add_subplot(413)
+drw.set_xlabel("angular frequency $\omega$")
+drw.set_ylabel("signal amplitude")
+drw.plot(W[:spectrumLimit], A[:spectrumLimit])
+
+drw = fig.add_subplot(414)
+drw.set_xlabel("time t")
+drw.set_ylabel("signal intensity")
+drw.plot(X, reconstruction)
 
 plt.show()
